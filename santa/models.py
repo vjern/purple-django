827ef00ee8d724fde1d441da19e696f669d82c3d
vjern <jocelyn.vernay@gmail.com>
2023-12-18 00:52:52 +0100

    Add history & draw models

--- santa/models.py
+++ santa/models.py
@@ -6,5 +6,15 @@ class User(models.Model):
 
 
 class Ban(models.Model):
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE)
-    target = models.IntegerField
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipient')
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target')
+
+
+class Draw(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class DrawHistory(models.Model):
+    draw = models.ForeignKey(User, on_delete=models.CASCADE)
+    giver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='giver')
+    taker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='taker')
