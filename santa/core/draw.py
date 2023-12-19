a5102eccdffcdbb515d00732e7b03dcdb4ed0ad0
vjern <jocelyn.vernay@gmail.com>
2023-12-19 17:37:20 +0100

    Simplify core logic

--- santa/core/draw.py
+++ santa/core/draw.py
@@ -1,36 +1,34 @@
-from dataclasses import dataclass
 import random
 
 
 UserId = int
-@dataclass
-class User:
-    id: UserId
-    name: str
-
-
-@dataclass
-class Exclusion:
-    recipient: str
-    target: str
 
 
 def generate_draw(
     users: list[UserId],
     exclusions: dict[UserId, set[UserId]],
 ) -> list[tuple[UserId, UserId]]:
+    """
+    From a list of users (their ids) and a list of exclusions (ie a blacklist),
+    compute pairs between users. A given user must appear exactly once as
+    left and right member, but not both in the same pair.
+    """
     pairs = []
     pool = list(range(len(users)))
     for i, u in enumerate(users):
         # draw another user as target of secret santa
-        if not pool: # no more users to draw from
-            raise RuntimeError('Pool to draw from is empty, try again or loosen exclusions')
+        if not pool:  # no more users to draw from
+            raise RuntimeError(
+                "Pool to draw from is empty, try again or loosen exclusions"
+            )
         target_idx = i
         attempts = 100
-        # TODO and also you can't be the target of someone you have in your own blacklist 
+        # TODO and also you can't be the target of someone you have in your own blacklist
         while target_idx == i or users[target_idx] in exclusions.get(u, []):
             if not attempts:
-                raise RuntimeError('Unable to draw after many attempts, try again or loosen exclusions')
+                raise RuntimeError(
+                    "Unable to draw after many attempts, try again or loosen exclusions"
+                )
             attempts -= 1
             target_idx_idx = random.randrange(len(pool))
             target_idx = pool[target_idx_idx]
